119f5d9d57dbf6748b68d84008661b9eab572c79
Hadrien Froger <hadrien@octree.ch>
2022-04-06 01:24:44 +0100

    chore: update to 0.24

--- lib/decidim/socio_demographic_authorization_handler/version.rb
+++ lib/decidim/socio_demographic_authorization_handler/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-meetings version.
   module SocioDemographicAuthorizationHandler
     def self.version
-      "0.23.6"
+      "0.24.0"
     end
 
     def self.decidim_version
-      "v0.23.6"
+      "v0.24.0"
     end
   end
 end
